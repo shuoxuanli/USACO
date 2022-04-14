090aa26b474ba77a2080ec5333484689dd05de9a
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 13:08:06 -0400

    Create just stalling.cpp

---
+++ Bronze/2020-2021/Jan/just stalling.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long cows[21], stalls[21], cnt[30];
+int main() {
+  int n;
+  cin>>n;
+  for(int i = 0; i < n; i++) {
+    cin>>cows[i];
+  }
+
+  for(int i = 0; i < n; i++) {
+    cin>>stalls[i];
+  }
+
+  sort(stalls, stalls+n);
+
+
+  for(int i = 0; i < n; i++) {
+    int count = 0;
+    for(int j = 0; j < n; j++) {
+      if(stalls[i] >= cows[j]) {
+        count++;
+      }
+    }
+    cnt[i] += count - i;
+    //cout<<cnt[i]<<endl;
+  }
+
+  long long ans = 1;
+  for(int i = 0; i < n; i++) { 
+    ans *= cnt[i];
+  }
+  
+  cout<<ans<<endl; 
+  return 0;
+} 
