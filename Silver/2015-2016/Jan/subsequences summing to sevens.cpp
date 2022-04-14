58d45991961eed7a35c725bb5ed18bacba1d6a1e
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 13:35:43 -0400

    Create subsequences summing to sevens.cpp

---
+++ Silver/2015-2016/Jan/subsequences summing to sevens.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long prefix[50001];
+
+int main() {
+
+  freopen("div7.in", "r", stdin);
+  freopen("div7.out", "w", stdout);
+
+  int n;
+  cin>>n;
+
+  for(int i = 1; i <= n; i++) {
+    long long seq;
+    cin>>seq;
+    prefix[i] = prefix[i-1] + seq;
+  }
+
+  int ans = 0;
+  int cnt = 0;
+  for(int i = 1; i <= n; i++) {
+    for(int j = i + cnt; j <= n; j++) {
+      long long tmp = prefix[j] - prefix[i-1];
+      if(tmp % 7 == 0) {
+        ans = max(j-i+1, ans);
+        cnt = ans - 1;
+      }
+    }
+  }
+
+  cout<<ans<<endl;
+  return 0;
+}
