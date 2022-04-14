b531486184cf064bbdaae9dce4f63f312e496fdc
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 11:45:44 -0400

    Create out of place v2.java

---
+++ Bronze/2017-2018/Jan/out of place v2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+  public static void main(String[] args) throws IOException{
+    BufferedReader in = new BufferedReader(new FileReader("outofplace.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("outofplace.out")));
+
+    int n = Integer.parseInt(in.readLine());
+
+    int[] h = new int[n];
+    for(int i = 0; i < n; i++) {
+      StringTokenizer st = new StringTokenizer(in.readLine());
+      h[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int diff = 0;
+    int[] h2 = new int[n];
+    for(int i = 0; i < n; i++) {
+      h2[i] = h[i];
+    }
+    Arrays.sort(h);
+
+    for(int i = 0; i < n; i++) {
+      if(h[i] != h2[i]) diff++;
+    }
+
+    out.println(diff-1);
+    in.close();
+    out.close();
+  }
+}
