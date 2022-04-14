e314c44883a8919ad7b5d9f043ff918c5e031be9
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 11:13:39 -0400

    Create fencepainting.java

---
+++ Bronze/2015-2016/Dec/fencepainting.java
@@ -0,0 +1,52 @@
+nitnimport java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		Kattio io = new Kattio("paint");
+		int a = io.nextInt();
+		int b = io.nextInt();
+		int c = io.nextInt();
+		int d = io.nextInt();
+    
+		// the sum of the two intervals
+		int total = (b - a) + (d - c);
+		// subtract the intersection
+		int intersection = Math.max(Math.min(b, d) - Math.max(a, c), 0);
+		int union = total - intersection;
+
+		io.println(union);
+		io.close();
+	}
+
+	static class Kattio extends PrintWriter {
+		private BufferedReader r;
+		private StringTokenizer st;
+	
+		// standard input
+		public Kattio() { this(System.in, System.out); }
+		public Kattio(InputStream i, OutputStream o) {
+			super(o);
+			r = new BufferedReader(new InputStreamReader(i));
+		}
+		// USACO-style file input
+		public Kattio(String problemName) throws IOException {
+			super(new FileWriter(problemName + ".out"));
+			r = new BufferedReader(new FileReader(problemName + ".in"));
+		}
+	
+		// returns null if no more input
+		public String next() {
+			try {
+				while (st == null || !st.hasMoreTokens())
+					st = new StringTokenizer(r.readLine());
+				return st.nextToken();
+			} catch (Exception e) { }
+			return null;
+		}
+	
+		public int nextInt() { return Integer.parseInt(next()); }
+		public double nextDouble() { return Double.parseDouble(next()); }
+		public long nextLong() { return Long.parseLong(next()); }
+	}
+}
