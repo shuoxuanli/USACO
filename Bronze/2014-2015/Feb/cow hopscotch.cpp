ea3844638184aca152922be8102c4b6d1e961aad
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 13:30:43 -0400

    Create cow hopscotch.cpp

---
+++ Bronze/2014-2015/Feb/cow hopscotch.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char grid[16][16];
+int ans = 0;
+int r, c;
+
+void dfs(int a, int b) {
+  if(a == r-1 && b == c-1) {
+    ans++;
+    return;
+  }
+  for(int i = a+1; i < r; i++) {
+    for(int j = b+1; j < c; j++) {
+      if(grid[i][j] != grid[a][b] && i != a && j != b) {
+        dfs(i, j);
+      }
+    }
+  }
+}
+
+int main() {
+  freopen("hopscotch.in", "r", stdin);
+  freopen("hopscotch.out", "w", stdout);
+
+  cin>>r>>c;
+
+  for(int i = 0; i < r; i++) {
+    for(int j = 0; j < c; j++) {
+      cin>>grid[i][j];
+    }
+  }
+  
+  dfs(0,0);
+  
+  cout<<ans<<endl;
+  return 0;
+} 
