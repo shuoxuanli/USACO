917987c9593b61c8a996d6a500ae33ef7437821d
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 11:57:26 -0400

    Create guess the animal.cpp

---
+++ Bronze/2018-2019/Jan/guess the animal.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string animals[101][101];
+int sk[101];
+
+int main() {
+
+  freopen("guess.in", "r", stdin);
+  freopen("guess.out", "w", stdout);
+
+  int n;
+  cin>>n;
+
+  unordered_map <string, int> c;
+
+  for(int i = 0; i < n; i++) {  
+    string animal;
+    int skills;
+    cin>>animal>>skills;
+    sk[i] = skills;
+    for(int j = 0; j < skills; j++) {
+      cin>>animals[i][j];
+    }
+  }
+
+  int ans = 0;
+  for(int i = 0; i < n; i++) {
+    for(int j = i+1; j < n; j++) {
+      int count = 0;
+      for(int k = 0; k < sk[i]; k++) {
+        for(int l = 0; l < sk[j]; l++) {
+          if(animals[i][k] == animals[j][l]) {
+            count++;
+          }
+        }
+      }
+      ans = max(count, ans);
+    }
+  }
+
+  cout<<ans+1<<endl;
+  return 0;
+} 
