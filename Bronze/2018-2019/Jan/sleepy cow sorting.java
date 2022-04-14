17f92cc5c0436b95269a9877f27802f084d305a7
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 11:56:53 -0400

    Create sleepy cow sorting.java

---
+++ Bronze/2018-2019/Jan/sleepy cow sorting.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+  public static void main(String[] args) throws IOException{
+    BufferedReader in = new BufferedReader(new FileReader("sleepy.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("sleepy.out")));
+
+    int n = Integer.parseInt(in.readLine());
+
+    int[] order = new int[n];
+    StringTokenizer st = new StringTokenizer(in.readLine());
+
+    for(int i = 0; i < n; i++) {
+      order[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int count = 1;
+    boolean sorted = false;
+    for(int i = n-1; i > 0; i--) {
+      if(order[i] > order[i-1]) {
+        sorted = false;
+        count++;
+        continue;
+      }
+      break;
+    }
+
+    if(!sorted) out.println(n-count);
+    else out.println(0);
+
+    in.close();
+    out.close();
+  }
+}
