d0be0e847b1f035fbba6eb1a58d92b588e13c41a
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 11:19:28 -0400

    Create promotion_counting.cpp

---
+++ Bronze/2015-2016/Jan/promotion_counting.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int input[4][2];
+
+int main() {
+  freopen("promote.in", "r", stdin);
+  freopen("promote.out", "w", stdout);
+
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 2; j++) {
+      cin>>input[i][j];
+    }
+  }
+
+  int gp = input[3][1] - input[3][0];
+  int sg = input[2][1] + gp - input[2][0];
+  int bs = input[1][1] + sg - input[1][0];
+
+  cout<<bs<<endl;
+  cout<<sg<<endl;
+  cout<<gp<<endl;
+
+} 
