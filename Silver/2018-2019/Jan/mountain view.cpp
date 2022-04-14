216ca1915621a821cd19db97d6a479b03acbcd7e
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 19:43:42 -0400

    Create mountain view.cpp

---
+++ Silver/2018-2019/Jan/mountain view.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct view{
+  int x, y, org;
+};
+
+view mountain[100001];
+
+bool cmp(const view&a, const view&b) {
+  return a.x < b.x;
+}
+
+int main() {
+  ifstream cin("mountains.in");
+  ofstream cout("mountains.out");
+
+  int n;
+  cin>>n;
+
+
+  for(int i = 0; i < n; i++) {
+    cin>>mountain[i].x>>mountain[i].y;
+    mountain[i].org = i;
+  }
+
+  sort(mountain, mountain+n, cmp);
+
+  int r = mountain[0].x + mountain[0].y, idx = 0;
+  map<int, int> ans; 
+  for(int i = 0; i < n; i++) {
+    if(i == idx) {
+      if(ans.count(mountain[i].org)) {
+        ans[mountain[i].org]++;
+      } else {
+        ans[mountain[i].org] = 1;
+      }
+    }
+    else if(mountain[i].x > r) {
+      if(ans.count(mountain[i].org)) {
+        ans[mountain[i].org]++;
+      } else {
+        ans[mountain[i].org] = 1;
+      }
+    }
+    else if(mountain[i].x <= r && mountain[i].y > abs(mountain[idx].x + mountain[idx].y - mountain[i].x)) {
+      if(ans.count(mountain[i].org)) {
+        ans[mountain[i].org]++;
+      } else {
+        ans[mountain[i].org] = 1;
+      }
+    }
+    if(mountain[i].x + mountain[i].y > r) {
+      r = mountain[i].x + mountain[i].y;
+      idx = i;
+    }
+  } 
+
+  
+  int l = mountain[n-1].x - mountain[n-1].y;
+  idx = n-1;
+  for(int i = n-1; i >= 0; i--) {
+    if(i == idx) {
+      if(ans.count(mountain[i].org)) {
+        ans[mountain[i].org]++;
+      } else {
+        ans[mountain[i].org] = 1;
+      }
+    }
+    else if(mountain[i].x < l) {
+      if(ans.count(mountain[i].org)) {
+        ans[mountain[i].org]++;
+      } else {
+        ans[mountain[i].org] = 1;
+      }
+    }
+    else if(mountain[i].x >= l && mountain[i].y > abs(mountain[i].x - (mountain[idx].x - mountain[idx].y))) {
+      if(ans.count(mountain[i].org)) {
+        ans[mountain[i].org]++;
+      } else {
+        ans[mountain[i].org] = 1;
+      }
+    }
+    if(mountain[i].x - mountain[i].y < l) {
+      l = mountain[i].x - mountain[i].y;
+      idx = i;
+    }
+  }
+
+  int cnt = 0;
+  for(auto x : ans){
+    if(x.second==2) cnt++;
+  }
+  cout<<cnt<<endl;
+  return 0;
+} 
