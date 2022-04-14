b74fdc59dd263661099eeb8618c074d2938da71e
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 13:37:53 -0400

    Update milk pails.cpp

--- Silver/2015-2016/Feb/milk pails.cpp
+++ Silver/2015-2016/Feb/milk pails.cpp
@@ -1,34 +1,61 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  freopen("pails.in", "r", stdin);
-  freopen("pails.out", "w", stdout);
-  
-  int x,y,m;
-  cin>>x>>y>>m;
+int x, y, k, m, ans = INT_MAX;
+bool vis[101][101];
 
-  if(m%x == 0 || m%y == 0 || m%(x+y)==0) {
-    cout<<m<<endl;
-    return 0;
-  }
+struct pails {
+  int a, b, c;
+};
 
-  int ans = 0;
-  for(int i = 0; i <= m/x; i++) {
-    for(int j = 0; j <= m/y; j++) {
-      int product = i*x + j*y;
-      if(product > m) {
-        break;
-      }
-      else if(product == m) {
-        cout<<m<<endl;
-        return 0;
-      }
-      else if (product < m){
-        ans = max(product, ans);
-      }
+void bfs(int u, int v) {
+  queue<pails> q;
+  q.push({u,v,0});
+  while(!q.empty()) {
+    int r = q.front().a;
+    int c = q.front().b;
+    int t = q.front().c;
+    q.pop();
+
+    if(vis[r][c] || t > k) {
+      continue;
     }
+
+    vis[r][c] = 1;
+    ans = min(ans, abs(m-r-c));
+
+    q.push({x,c,t+1});
+    q.push({r,y,t+1});
+    q.push({0,c,t+1});
+    q.push({r,0,t+1});
+
+    int tr = r;
+    int tc = c;
+
+    int dec = min(x-r, c);
+    tc -= dec;
+    tr += dec;
+    q.push({tr, tc, t+1});
+
+    tr = r;
+    tc = c;
+
+    dec = min(y-c, r);
+    tr -= dec;
+    tc += dec;
+    q.push({tr, tc, t+1});
   }
+}
+
+int main() {
+  ifstream cin("pails.in");
+  ofstream cout("pails.out");
+  
+  cin>>x>>y>>k>>m;
+
+  bfs(0,0);
+
   cout<<ans<<endl;
+  
   return 0;
 } 
