36c2fc7884ef045d6b9785cb304f974ce6531e3e
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 11:48:35 -0400

    Create teleportation.cpp

---
+++ Bronze/2017-2018/Feb/teleportation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  freopen("teleport.in", "r", stdin);
+  freopen("teleport.out", "w", stdout);
+
+  int a, b, x, y;
+  cin>>a>>b>>x>>y;
+
+  int d1 = max(b,a) - min(b,a);
+  int d2 = abs(min(x,y)-min(b,a)) + abs(max(b,a)-max(x,y));
+
+  int ans = min(d1, d2);
+
+  cout<<ans<<endl;
+} 
