573a45d19dc584fdf4d5250decfc6da89c58d7f8
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 13:42:21 -0400

    Create counting haybales v2.cpp

---
+++ Silver/2016-2017/Dec/counting haybales v2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ifstream cin("haybales.in");
+  ofstream cout("haybales.out");
+  
+  int n, q;
+  cin>>n>>q;
+
+  vector<int> bales(n);
+  for(int i = 0; i < n; i++) {
+    cin>>bales[i];
+  }
+
+  sort(begin(bales), end(bales));
+
+  for(int i = 0; i < q; i++) {
+    int a, b; 
+    cin>>a>>b;
+    auto hi = upper_bound(begin(bales), end(bales), b);
+    auto lo = lower_bound(begin(bales), end(bales), a);
+    cout<<hi-lo<<endl;
+  }
+
+  return 0;
+}
