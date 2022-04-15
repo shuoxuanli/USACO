6c52ca9e2c07b98952df6d11ab70e8233a20681b
shuoxuanli <103616713+shuoxuanli@users.noreply.github.com>
2022-04-14 20:04:27 -0400

    Update berry picking.cpp

--- Silver/2019-2020/Jan/berry picking.cpp
+++ Silver/2019-2020/Jan/berry picking.cpp
@@ -6,37 +6,37 @@ int a[100000];
 int mod;
  
 bool cmp(int a,int b) {
-	return (a%mod) > (b%mod);
+  return (a%mod) > (b%mod);
 }
  
 int main() {
-	ifstream cin("berries.in");
+  ifstream cin("berries.in");
   ofstream cout("berries.out");
   
-	cin >> n >> k;
-	int m = 0;
-	for(int i=0;i<n;i++) {
-		cin >> a[i];
-		m = max(m, a[i]);
-	}
+  cin >> n >> k;
+  int m = 0;
+  for(int i=0;i<n;i++) {
+    cin >> a[i];
+    m = max(m, a[i]);
+  }
   
-	int best = 0;
-	for(int b=1;b <= m; b++) {
-		int full = 0;
-		for(int i=0;i<n;i++)
-			full += a[i]/b;
-		if(full < k/2)
-			break;
-		if(full >= k) {
-			best = max(best, b*(k/2));
-			continue;
-		}
-		mod = b;
-		sort(a, a+n, cmp);
-		int cur = b*(full - k/2);
-		for(int i=0;i<n && i+full<k;i++)
-			cur += a[i]%b;
-		best = max(best,cur);
-	}
-	cout << best << '\n';
+  int best = 0;
+  for(int b=1;b <= m; b++) {
+    int full = 0;
+    for(int i=0;i<n;i++)
+      full += a[i]/b;
+      if(full < k/2)
+	break;
+      if(full >= k) {
+        best = max(best, b*(k/2));
+	continue;
+      }
+      mod = b;
+      sort(a, a+n, cmp);
+      int cur = b*(full - k/2);
+      for(int i=0;i<n && i+full<k;i++)
+        cur += a[i]%b;
+	best = max(best,cur);
+      }
+   cout << best << '\n';
 }
