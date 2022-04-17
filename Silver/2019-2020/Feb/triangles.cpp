d0da439af105a4c3986409b55523b6580ab10353
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-04-17 02:11:42 -0400

    Update triangles.cpp

--- Silver/2019-2020/Feb/triangles.cpp
+++ Silver/2019-2020/Feb/triangles.cpp
@@ -7,12 +7,6 @@ typedef long long ll;
 
 const int MOD = 1e9+7; 
 
-void setIO(string s) {
-  ios_base::sync_with_stdio(0); cin.tie(0); 
-  freopen((s+".in").c_str(),"r",stdin);
-  freopen((s+".out").c_str(),"w",stdout);
-}
-
 struct mi {
   int v; explicit operator int() const { return v; }
   mi(ll _v) : v(_v%MOD) { v += (v<0)*MOD; }
@@ -28,32 +22,33 @@ vector<mi> sum[100005];
 vector<pair<int,int>> todo[20001];
  
 void check() {
-	for (int i = 0; i <= 20000; ++i) if (todo[i].size() > 0) {
-		int sz = todo[i].size();
-		sort(begin(todo[i]),end(todo[i]));
-		mi cur = 0; 
-		for (int j = 0; j < sz; ++j) 
-			cur = cur+todo[i][j].f-todo[i][0].f;
-		for (int j = 0; j < sz; ++j) {
-			if (j) cur = cur+(2*j-sz)*(todo[i][j].f-todo[i][j-1].f);
-			sum[todo[i][j].s].push_back(cur);
-		}
-	}
+  for (int i = 0; i <= 20000; ++i) if (todo[i].size() > 0) {
+    int sz = todo[i].size();
+    sort(begin(todo[i]),end(todo[i]));
+    mi cur = 0; 
+    for (int j = 0; j < sz; ++j) 
+      cur = cur+todo[i][j].f-todo[i][0].f;
+    for (int j = 0; j < sz; ++j) {
+      if (j) cur = cur+(2*j-sz)*(todo[i][j].f-todo[i][j-1].f);
+      sum[todo[i][j].s].push_back(cur);
+    }
+  }
 }
  
 int main() {
-	setIO("triangles"); 
-	cin >> N; v.resize(N); 
-	for (int i = 0; i < N; ++i) cin >> v[i].f >> v[i].s;
-	for (int i = 0; i <= 20000; ++i) todo[i].clear();
-	for (int i = 0; i < N; ++i) 
-		todo[v[i].f+10000].push_back({v[i].s,i});
-	check();
-	for (int i = 0; i <= 20000; ++i) todo[i].clear();
-	for (int i = 0; i < N; ++i) 
-		todo[v[i].s+10000].push_back({v[i].f,i});
-	check();
-	mi ans = 0; 
-	for (int i = 0; i < N; ++i) ans = ans+sum[i][0]*sum[i][1];
-	cout << ans.v << "\n";
+  ifstream cin("triangles.in");
+  ofstream cout("triangles.out");
+  cin >> N; v.resize(N); 
+  for (int i = 0; i < N; ++i) cin >> v[i].f >> v[i].s;
+  for (int i = 0; i <= 20000; ++i) todo[i].clear();
+  for (int i = 0; i < N; ++i) 
+    todo[v[i].f+10000].push_back({v[i].s,i});
+  check();
+  for (int i = 0; i <= 20000; ++i) todo[i].clear();
+  for (int i = 0; i < N; ++i) 
+    todo[v[i].s+10000].push_back({v[i].f,i});
+  check();
+  mi ans = 0; 
+  for (int i = 0; i < N; ++i) ans = ans+sum[i][0]*sum[i][1];
+  cout << ans.v << "\n";
 }
