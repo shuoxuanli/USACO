bd1a3570458ec690c38e0f26af9badc5032f3c96
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-04-19 13:32:11 -0400

    Update sleepy cow sorting.java

--- Bronze/2018-2019/Jan/sleepy cow sorting.java
+++ Bronze/2018-2019/Jan/sleepy cow sorting.java
@@ -4,7 +4,7 @@ import java.io.*;
 class Main {
   public static void main(String[] args) throws IOException{
     BufferedReader in = new BufferedReader(new FileReader("sleepy.in"));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("sleepy.out")));
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("sleepy.out")));
 
     int n = Integer.parseInt(in.readLine());
 
