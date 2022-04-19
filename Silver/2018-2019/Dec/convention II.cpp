af1a93da212be8f334c58101aab6b8a1398b8aab
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-04-19 14:07:09 -0400

    Update convention II.cpp

--- Silver/2018-2019/Dec/convention II.cpp
+++ Silver/2018-2019/Dec/convention II.cpp
@@ -2,38 +2,38 @@
 using namespace std;
 
 int main() {
-	freopen("convention2.in", "r", stdin);
-	freopen("convention2.out", "w", stdout);
+  freopen("convention2.in", "r", stdin);
+  freopen("convention2.out", "w", stdout);
 	
-	int n;
-	vector<vector<int>> cows;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int start, duration;
-		cin >> start >> duration;
-		cows.push_back({i, start, duration});
-	}
-
-	sort(cows.begin(), cows.end(), [](const vector<int> &a, const vector<int> &b) {
-		return a[1] < b[1];
-	});
-
-	int time = 0, cur = 0, ans = 0;
+  int n;
+  vector<vector<int>> cows;
+  cin >> n;
 	
-	set<vector<int>> waiting;
-	while (cur < n || waiting.size() > 0) {
-		if (cur < n && cows[cur][1] <= time) {
-			waiting.insert(cows[cur]);
-			cur++;
-		} else if (waiting.size() == 0) {
-			time = cows[cur][1] + cows[cur][2];
-			cur++;
-		} else {
-			vector<int> next = *waiting.begin();
-			ans = max(ans, time - next[1]);
-			time += next[2];
-			waiting.erase(waiting.begin());
-		}
-	}
-	cout << ans << '\n';
+  for (int i = 0; i < n; i++) {
+    int start, duration;
+    cin >> start >> duration;
+    cows.push_back({i, start, duration});
+  }
+
+  sort(cows.begin(), cows.end(), [](const vector<int> &a, const vector<int> &b) {
+    return a[1] < b[1];
+  });
+
+  int time = 0, cur = 0, ans = 0;
+  set<vector<int>> waiting;
+  while (cur < n || waiting.size() > 0) {
+    if (cur < n && cows[cur][1] <= time) {
+      waiting.insert(cows[cur]);
+      cur++;
+    } else if (waiting.size() == 0) {
+      time = cows[cur][1] + cows[cur][2];
+      cur++;
+    } else {
+      vector<int> next = *waiting.begin();
+      ans = max(ans, time - next[1]);
+      time += next[2];
+      waiting.erase(waiting.begin());
+    }
+  }
+  cout << ans << '\n';
 }
