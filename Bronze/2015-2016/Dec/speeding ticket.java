74783b1720c965c7dc882a49278e825b257db897
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-04-19 10:44:33 -0400

    Update speeding ticket.java

--- Bronze/2015-2016/Dec/speeding ticket.java
+++ Bronze/2015-2016/Dec/speeding ticket.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.util.Arrays;
 
 class Main {
   public static void main(String[] args) throws IOException {
@@ -76,33 +75,31 @@ class Main {
   }
 
   static class Kattio extends PrintWriter {
-		private BufferedReader r;
-		private StringTokenizer st;
+    private BufferedReader r;
+    private StringTokenizer st;
 	
-		// standard input
-		public Kattio() { this(System.in, System.out); }
-		public Kattio(InputStream i, OutputStream o) {
-			super(o);
-			r = new BufferedReader(new InputStreamReader(i));
-		}
-		// USACO-style file input
-		public Kattio(String problemName) throws IOException {
-			super(new FileWriter(problemName + ".out"));
-			r = new BufferedReader(new FileReader(problemName + ".in"));
-		}
-	
-		// returns null if no more input
-		public String next() {
-			try {
-				while (st == null || !st.hasMoreTokens())
-					st = new StringTokenizer(r.readLine());
-				return st.nextToken();
-			} catch (Exception e) { }
-			return null;
-		}
-	
-		public int nextInt() { return Integer.parseInt(next()); }
-		public double nextDouble() { return Double.parseDouble(next()); }
-		public long nextLong() { return Long.parseLong(next()); }
-	}
+    public Kattio() { this(System.in, System.out); }
+    public Kattio(InputStream i, OutputStream o) {
+      super(o);
+      r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public Kattio(String problemName) throws IOException {
+      super(new FileWriter(problemName + ".out"));
+      r = new BufferedReader(new FileReader(problemName + ".in"));
+    }
+
+    public String next() {
+      try {
+        while (st == null || !st.hasMoreTokens())
+        st = new StringTokenizer(r.readLine());
+        return st.nextToken();
+      } catch (Exception e) { }
+      return null;
+    }
+
+    public int nextInt() { return Integer.parseInt(next()); }
+    public double nextDouble() { return Double.parseDouble(next()); }
+    public long nextLong() { return Long.parseLong(next()); }
+  }
 }
