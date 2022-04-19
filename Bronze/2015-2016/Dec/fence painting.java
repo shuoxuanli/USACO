9864ba51adf7d56a9d0d08428e59f254b17ccaf1
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-04-19 10:42:43 -0400

    Update fence painting.java

--- Bronze/2015-2016/Dec/fence painting.java
+++ Bronze/2015-2016/Dec/fence painting.java
@@ -9,41 +9,40 @@ class Main {
     int c = io.nextInt();
     int d = io.nextInt();
     
-		int total = (b - a) + (d - c);
+    int total = (b - a) + (d - c);
+    int intersection = Math.max(Math.min(b, d) - Math.max(a, c), 0);
+    int union = total - intersection;
 
-		int intersection = Math.max(Math.min(b, d) - Math.max(a, c), 0);
-		int union = total - intersection;
+    io.println(union);
+    io.close();
+  }
 
-		io.println(union);
-		io.close();
-	}
-
-	static class Kattio extends PrintWriter {
-		private BufferedReader r;
-		private StringTokenizer st;
+  static class Kattio extends PrintWriter {
+    private BufferedReader r;
+    private StringTokenizer st;
 	
-		public Kattio() { this(System.in, System.out); }
-		public Kattio(InputStream i, OutputStream o) {
-			super(o);
-			r = new BufferedReader(new InputStreamReader(i));
-		}
+    public Kattio() { this(System.in, System.out); }
+    public Kattio(InputStream i, OutputStream o) {
+      super(o);
+      r = new BufferedReader(new InputStreamReader(i));
+    }
 
-		public Kattio(String problemName) throws IOException {
-			super(new FileWriter(problemName + ".out"));
-			r = new BufferedReader(new FileReader(problemName + ".in"));
-		}
-	
-		public String next() {
-			try {
-				while (st == null || !st.hasMoreTokens())
-					st = new StringTokenizer(r.readLine());
-				return st.nextToken();
-			} catch (Exception e) { }
-			return null;
-		}
-	
-		public int nextInt() { return Integer.parseInt(next()); }
-		public double nextDouble() { return Double.parseDouble(next()); }
-		public long nextLong() { return Long.parseLong(next()); }
-	}
+    public Kattio(String problemName) throws IOException {
+      super(new FileWriter(problemName + ".out"));
+      r = new BufferedReader(new FileReader(problemName + ".in"));
+    }
+
+    public String next() {
+      try {
+        while (st == null || !st.hasMoreTokens())
+        st = new StringTokenizer(r.readLine());
+        return st.nextToken();
+      } catch (Exception e) { }
+      return null;
+    }
+
+    public int nextInt() { return Integer.parseInt(next()); }
+    public double nextDouble() { return Double.parseDouble(next()); }
+    public long nextLong() { return Long.parseLong(next()); }
+  }
 }
