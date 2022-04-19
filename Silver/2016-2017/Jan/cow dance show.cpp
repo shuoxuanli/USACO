8439524d60b4371c3c1b2db2e3eedd08bf5f83f9
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-04-19 14:02:08 -0400

    Update cow dance show.cpp

--- Silver/2016-2017/Jan/cow dance show.cpp
+++ Silver/2016-2017/Jan/cow dance show.cpp
@@ -5,10 +5,10 @@ int n, tmax;
 int t[10001];
 
 bool check(int k) {
-	 int a[k];
-   for(int i = 0; i < k; i++) {
-     a[i] = t[i];
-   }
+  int a[k];
+  for(int i = 0; i < k; i++) {
+    a[i] = t[i];
+  }
 
   for(int i = k; i < n; i++) {
     sort(a, a+k);
@@ -20,27 +20,27 @@ bool check(int k) {
 }
 
 int main() {
-	freopen("cowdance.in","r",stdin);
-	freopen("cowdance.out","w",stdout);
+  freopen("cowdance.in","r",stdin);
+  freopen("cowdance.out","w",stdout);
 	
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  cin>>n>>tmax;
+  for(int i=0; i<n; i++) {
+    cin>>t[i];
+  }
 	
-	cin>>n>>tmax;
-	for(int i=0; i<n; i++) {
-		cin>>t[i];
-	}
-	int left = 1, right = n, ans = n;
-	while(left<=right) {
-		int mid = (left+right)/2;
-		if(check(mid)) {
+  int left = 1, right = n, ans = n;
+  while(left<=right) {
+    int mid = (left+right)/2;
+    if(check(mid)) {
       ans = mid;
       right = mid-1;
     } else {
       left = mid+1;
     }
-	}
-	cout<<ans<<endl;
-
-	return 0;
+  }
+  cout<<ans<<endl;
+  return 0;
 }
