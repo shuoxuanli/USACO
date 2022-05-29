c6f54ce31b6919e3a9635c4447497d11a6c1dbe1
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-05-29 16:26:47 -0400

    Create photoshoot.cpp

---
+++ Bronze/2021-2022/Open/photoshoot.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	string s;
+	cin>>s;
+		
+	int ans = 0;
+	for(int i = n-1; i >= 1; i -= 2) {
+		if(s[i] == s[i-1]) continue;
+		if(s[i] == 'G' && ans%2==1) ans++;
+		if(s[i] == 'H' && ans%2==0) ans++;
+	}
+
+	cout<<ans<<endl;
+}
