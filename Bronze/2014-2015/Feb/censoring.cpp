5344ee2785ab60c0bbc126adc739d6650c4b47d2
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-03 18:28:40 -0400

    Update censoring.cpp

--- Bronze/2014-2015/Feb/censoring.cpp
+++ Bronze/2014-2015/Feb/censoring.cpp
@@ -4,22 +4,21 @@ using namespace std;
 using namespace std;
 
 int main() {
-  freopen("censor.in", "r", stdin);
-  freopen("censor.out", "w", stdout);
+	freopen("censor.in", "r", stdin);
+	freopen("censor.out", "w", stdout);
 
-  string S, T;
-  cin >> S >> T;
+	string S, T;
+	cin >> S >> T;
 
-  string R;
-  for (int i = 0; i < S.size(); i++) {
-    R += S[i];
+	string R;
+	for (int i = 0; i < S.size(); i++) {
+		R += S[i];
 
-    if (R.size() >= T.size() && R.substr(R.size() - T.size()) == T) {
-      R.resize(R.size() - T.size());
-    }
-  }
+		if (R.size() >= T.size() && R.substr(R.size() - T.size()) == T) {
+			R.resize(R.size() - T.size());
+		}
+	}
 
-  cout << R << endl;
-
-  return 0;
+	cout << R << endl;
+	return 0;
 }
