60cfdf2313a216a065e84af5cb31f06940f90bd7
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-03 18:29:39 -0400

    Update cow hopscotch.cpp

--- Bronze/2014-2015/Feb/cow hopscotch.cpp
+++ Bronze/2014-2015/Feb/cow hopscotch.cpp
@@ -6,33 +6,33 @@ int ans = 0;
 int r, c;
 
 void dfs(int a, int b) {
-  if(a == r-1 && b == c-1) {
-    ans++;
-    return;
-  }
-  for(int i = a+1; i < r; i++) {
-    for(int j = b+1; j < c; j++) {
-      if(grid[i][j] != grid[a][b] && i != a && j != b) {
-        dfs(i, j);
-      }
-    }
-  }
+	if(a == r-1 && b == c-1) {
+		ans++;
+		return;
+	}
+	for(int i = a+1; i < r; i++) {
+		for(int j = b+1; j < c; j++) {
+			if(grid[i][j] != grid[a][b] && i != a && j != b) {
+				dfs(i, j);
+			}
+		}
+	}
 }
 
 int main() {
-  freopen("hopscotch.in", "r", stdin);
-  freopen("hopscotch.out", "w", stdout);
+	freopen("hopscotch.in", "r", stdin);
+	freopen("hopscotch.out", "w", stdout);
 
-  cin>>r>>c;
+	cin>>r>>c;
 
-  for(int i = 0; i < r; i++) {
-    for(int j = 0; j < c; j++) {
-      cin>>grid[i][j];
-    }
-  }
-  
-  dfs(0,0);
-  
-  cout<<ans<<endl;
-  return 0;
+	for(int i = 0; i < r; i++) {
+		for(int j = 0; j < c; j++) {
+			cin>>grid[i][j];
+		}
+	}
+
+	dfs(0,0);
+
+	cout<<ans<<endl;
+	return 0;
 } 
