de0534efa26c9fa893b903bbc807463350875adc
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-03 18:26:42 -0400

    Update learning by example.cpp

--- Bronze/2014-2015/Dec/learning by example.cpp
+++ Bronze/2014-2015/Dec/learning by example.cpp
@@ -15,47 +15,47 @@ typedef long long ll;
 typedef pair<ll, ll> pll;
 
 int main() {
-  freopen("learning.in", "r", stdin);
-  freopen("learning.out", "w", stdout);
+	freopen("learning.in", "r", stdin);
+	freopen("learning.out", "w", stdout);
 
-  ll n, A, B;
-  cin>>n>>A>>B;
-  vector<pll> V;
-  for(ll i=0; i<n; i++) {
-    char buf[100];
-    ll w;
-    cin>>buf>>w;
-    V.PB(MP(w, buf[0]=='S' ? 1 : 0));
-  }
-  ll INF = ll(1000)*1000*1000*1000;
-  V.PB(MP(-INF, 0));
-  V.PB(MP(INF, 0));
-  sort(V.begin(), V.end());
+	ll n, A, B;
+	cin>>n>>A>>B;
+	vector<pll> V;
+	for(ll i=0; i<n; i++) {
+		char buf[100];
+		ll w;
+		cin>>buf>>w;
+		V.PB(MP(w, buf[0]=='S' ? 1 : 0));
+	}
+	ll INF = ll(1000)*1000*1000*1000;
+	V.PB(MP(-INF, 0));
+	V.PB(MP(INF, 0));
+	sort(V.begin(), V.end());
 
-  ll ans = 0;
-  for(ll i=0; i+1<V.size(); i++) {
-    ll S = V[i].X;
-    ll E = V[i+1].X;
-    ll M = (S+E)/2;
+	ll ans = 0;
+	for(ll i=0; i+1<V.size(); i++) {
+		ll S = V[i].X;
+		ll E = V[i+1].X;
+		ll M = (S+E)/2;
 
-    if(V[i].Y==1) {
-      ll s = max(A, S+1);
-      ll e = min(B, M);
-      if(e >= s) {
-        ans += e-s+1;
-      }
-    }
-    if(V[i+1].Y==1) {
-      ll s = max(A, M+1);
-      ll e = min(B, E);
-      if(e >= s) {
-        ans += e-s+1;
-      }
-    }
-    if(V[i+1].Y==1 && V[i].Y==0 && (S%2)==(E%2) && A<=M && M<=B) {
-      ans++; 
-    }
-  }
-  cout<<ans<<endl;
-  return 0;
+		if(V[i].Y==1) {
+			ll s = max(A, S+1);
+			ll e = min(B, M);
+			if(e >= s) {
+				ans += e-s+1;
+			}
+		}
+		if(V[i+1].Y==1) {
+			ll s = max(A, M+1);
+			ll e = min(B, E);
+			if(e >= s) {
+				ans += e-s+1;
+			}
+		}
+		if(V[i+1].Y==1 && V[i].Y==0 && (S%2)==(E%2) && A<=M && M<=B) {
+			ans++; 
+		}
+	}
+	cout<<ans<<endl;
+	return 0;
 }
