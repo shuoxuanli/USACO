e6cb547a334e5c75cfa2d829a4ec9a95f7cd5186
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-04 15:29:05 -0400

    Update drought.cpp

--- Bronze/2021-2022/Jan/drought.cpp
+++ Bronze/2021-2022/Jan/drought.cpp
@@ -1,42 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-using i64 = int64_t;
- 
-i64 solve(const vector<i64>& H) {
-  const int N = (int)H.size();
-  i64 f = 0;
-  for (int i = 0; i < N; ++i)
-    f += (i%2 == 0 ? 1 : -1)*H[i];
-    if (N%2 == 0) {
-      if (f != 0) return -1;
-    } else {
-      if (f < 0) return -1;
-    }
-  i64 last_o = 0;
-  vector<i64> o(N-1);
-  for (int i = 0; i+1 < N; ++i) {
-    last_o = o[i] = H[i]-f-last_o;
-    if (o[i] < 0) return -1;
-  }
-  if (N%2 == 0) {
-    i64 mn = o[0];
-    for (int i = 0; i < N; i += 2)
-      mn = min(mn,o[i]);
-    for (int i = 0; i < N; i += 2)
-      o[i] -= mn;
-  }
-  i64 sum_o = 0;
-  for (i64 i: o) sum_o += i;
-  return 2*sum_o;
-}
- 
-int main() {
-  int t; cin >> t;
-  while (t--) {
-    int N; cin >> N;
-    vector<i64> H(N);
-    for (auto& i: H) cin >> i;
-    cout << solve(H) << "\n";
-  }
+
+#define ll long long
+#define sz(x) (int)(x.size())
+
+int t, n;
+ll a[1000001], diff[100001];
+
+int main() {	
+	cin>>t;
+	while(t--) {
+		cin>>n;
+		for(int i = 0; i < n; i++) 
+			cin>>a[i];
+
+		ll d = 0; 
+		for(int i = 0; i < n; i++) {
+			if(i&1) d -= a[i];
+			else d += a[i];
+		}
+	
+		bool b = 1;
+		for(int i = 0; i < n; i++) {
+			diff[i] = a[i] - d;
+		}
+
+		for(int i = 1; i < n; i++) {
+			diff[i] -= diff[i-1];
+			b &= diff[i] >= 0;
+		}
+
+		if(n&1)
+			b &= d >= 0;
+		else 
+			b &= d == 0;
+	
+		if(n&1^1) {
+			ll mn = LLONG_MAX;
+			for(int i = 0; i < n; i += 2) 
+				mn = min(mn, diff[i]);
+			for(int i = 0; i < n; i += 2) 
+				diff[i] -= mn;
+		}
+
+		ll ans = accumulate(diff, diff+n, 0ll)*2;
+
+		cout<<(b ? ans : -1)<<endl;
+	}
 }
