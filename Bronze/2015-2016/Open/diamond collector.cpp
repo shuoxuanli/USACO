96f0e494b31afdeea08d1a865834c32dc0fc8f6f
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-05 02:38:08 -0400

    Update diamond collector.cpp

--- Bronze/2015-2016/Open/diamond collector.cpp
+++ Bronze/2015-2016/Open/diamond collector.cpp
@@ -4,31 +4,31 @@ using namespace std;
 int diamonds[1001];
 
 int main() {
-	freopen("diamond.in", "r", stdin);
-	freopen("diamond.out", "w", stdout);
+  freopen("diamond.in", "r", stdin);
+  freopen("diamond.out", "w", stdout);
 
-	int n, k;
-	cin>>n>>k;
+  int n, k;
+  cin>>n>>k;
 
-	for(int i = 0; i < n; i++) {
-		cin>>diamonds[i];
-	}
+  for(int i = 0; i < n; i++) {
+    cin>>diamonds[i];
+  }
 
-	sort(diamonds, diamonds + n);
+  sort(diamonds, diamonds + n);
 
-	int ans = 0;
-	for(int j = 0; j < n; j++) {
-		int temp = 1;
-		for(int i = j+1; i < n; i++) {
-			if(diamonds[i] - diamonds[j] <= k) {
-				temp++;
-			} else {
-				break;
-			}
-		}
-		ans = max(temp, ans);
-	}
+  int ans = 0;
+  for(int j = 0; j < n; j++) {
+    int temp = 1;
+    for(int i = j+1; i < n; i++) {
+      if(diamonds[i] - diamonds[j] <= k) {
+        temp++;
+      } else {
+        break;
+      }
+    }
+    ans = max(temp, ans);
+  }
 
-	cout<<ans<<endl;
-	return 0;
+  cout<<ans<<endl;
+  return 0;
 } 
