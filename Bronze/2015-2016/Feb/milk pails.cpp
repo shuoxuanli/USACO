94a81f3358c7dc4707dd5ede2eb70c2cc9f9875a
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-04 22:19:34 -0400

    Update milk pails.cpp

--- Bronze/2015-2016/Feb/milk pails.cpp
+++ Bronze/2015-2016/Feb/milk pails.cpp
@@ -2,33 +2,33 @@
 using namespace std;
 
 int main() {
-  freopen("pails.in", "r", stdin);
-  freopen("pails.out", "w", stdout);
-  
-  int x,y,m;
-  cin>>x>>y>>m;
+	freopen("pails.in", "r", stdin);
+	freopen("pails.out", "w", stdout);
 
-  if(m%x == 0 || m%y == 0 || m%(x+y)==0) {
-    cout<<m<<endl;
-    return 0;
-  }
+	int x,y,m;
+	cin>>x>>y>>m;
 
-  int ans = 0;
-  for(int i = 0; i <= m/x; i++) {
-    for(int j = 0; j <= m/y; j++) {
-      int product = i*x + j*y;
-      if(product > m) {
-        break;
-      }
-      else if(product == m) {
-        cout<<m<<endl;
-        return 0;
-      }
-      else if (product < m){
-        ans = max(product, ans);
-      }
-    }
-  }
-  cout<<ans<<endl;
-  return 0;
+	if(m%x == 0 || m%y == 0 || m%(x+y)==0) {
+		cout<<m<<endl;
+		return 0;
+	}
+
+	int ans = 0;
+	for(int i = 0; i <= m/x; i++) {
+		for(int j = 0; j <= m/y; j++) {
+			int product = i*x + j*y;
+			if(product > m) {
+				break;
+			}
+			else if(product == m) {
+				cout<<m<<endl;
+				return 0;
+			}
+			else if (product < m){
+				ans = max(product, ans);
+			}
+		}
+	}
+	cout<<ans<<endl;
+	return 0;
 } 
