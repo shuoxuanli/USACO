82d1b4a6177ec4f1e8ed293ddd9ddff58f7f12af
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-04 23:18:42 -0400

    Update drought.cpp

--- Bronze/2021-2022/Jan/drought.cpp
+++ Bronze/2021-2022/Jan/drought.cpp
@@ -14,37 +14,37 @@ int main() {
 		for(int i = 0; i < n; i++) 
 			cin>>a[i];
 
-			ll d = 0; 
-			for(int i = 0; i < n; i++) {
-				if(i&1) d -= a[i];
-				else d += a[i];
-			}
+		ll d = 0; 
+		for(int i = 0; i < n; i++) {
+			if(i&1) d -= a[i];
+			else d += a[i];
+		}
+	
+		bool b = 1;
+		for(int i = 0; i < n; i++) {
+			diff[i] = a[i] - d;
+		}
 
-			bool b = 1;
-			for(int i = 0; i < n; i++) {
-				diff[i] = a[i] - d;
-			}
+		for(int i = 1; i < n; i++) {
+			diff[i] -= diff[i-1];
+			b &= diff[i] >= 0;
+		}
 
-			for(int i = 1; i < n; i++) {
-				diff[i] -= diff[i-1];
-				b &= diff[i] >= 0;
-			}
+		if(n&1)
+			b &= d >= 0;
+		else 
+			b &= d == 0;
+	
+		if((n&1)^1) {
+			ll mn = LLONG_MAX;
+			for(int i = 0; i < n; i += 2) 
+				mn = min(mn, diff[i]);
+			for(int i = 0; i < n; i += 2) 
+				diff[i] -= mn;
+		}
 
-			if(n&1)
-				b &= d >= 0;
-			else 
-				b &= d == 0;
+		ll ans = accumulate(diff, diff+n, 0ll)*2;
 
-			if((n&1)^1) {
-				ll mn = LLONG_MAX;
-				for(int i = 0; i < n; i += 2) 
-					mn = min(mn, diff[i]);
-				for(int i = 0; i < n; i += 2) 
-					diff[i] -= mn;
-			}
-
-			ll ans = accumulate(diff, diff+n, 0ll)*2;
-
-			cout<<(b ? ans : -1)<<endl;
+		cout<<(b ? ans : -1)<<endl;
 	}
 }
