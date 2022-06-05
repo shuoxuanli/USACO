3c5baf0f19bd076e156578da4103143613ba5161
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-05 02:38:27 -0400

    Update field reduction.cpp

--- Bronze/2015-2016/Open/field reduction.cpp
+++ Bronze/2015-2016/Open/field reduction.cpp
@@ -2,77 +2,77 @@
 using namespace std;
 
 struct coor{
-	int x, y;
+  int x, y;
 };
 
 coor pt[50001];
 
 bool cmpx(coor a, coor b) {
-	return a.x > b.x;
+  return a.x > b.x;
 }
 
 bool cmpy(coor a, coor b) {
-	return a.y > b.y;
+  return a.y > b.y;
 }
 
 int main() {
-	freopen("reduce.in", "r", stdin);
-	freopen("reduce.out", "w", stdout);
+  freopen("reduce.in", "r", stdin);
+  freopen("reduce.out", "w", stdout);
 
-	int n;
-	cin>>n;
+  int n;
+  cin>>n;
 
-	for(int i = 0; i < n; i++) {
-		cin>>pt[i].x>>pt[i].y;
-	}
+  for(int i = 0; i < n; i++) {
+    cin>>pt[i].x>>pt[i].y;
+  }
 
-	sort(pt, pt+n, cmpx);
+  sort(pt, pt+n, cmpx);
 
-	int left = INT_MAX, right = 0;
-	int top = 0, bottom = INT_MAX;
-	for(int i = 1; i < n; i++) {
-		left = min(pt[i].x, left);
-		right = max(pt[i].x, right);
-		top = max(pt[i].y, top);
-		bottom = min(pt[i].y, bottom);
-	}
+  int left = INT_MAX, right = 0;
+  int top = 0, bottom = INT_MAX;
+  for(int i = 1; i < n; i++) {
+    left = min(pt[i].x, left);
+    right = max(pt[i].x, right);
+    top = max(pt[i].y, top);
+    bottom = min(pt[i].y, bottom);
+  }
+  
+  int area = abs(right-left)*abs(top-bottom);
 
-	int area = abs(right-left)*abs(top-bottom);
+  left = INT_MAX, right = 0;
+  top = 0, bottom = INT_MAX;
+  for(int i = 0; i < n-1; i++) {
+    left = min(pt[i].x, left);
+    right = max(pt[i].x, right);
+    top = max(pt[i].y, top);
+    bottom = min(pt[i].y, bottom);
+  }
+  area = min(abs(right-left)*abs(top-bottom), area);
 
-	left = INT_MAX, right = 0;
-	top = 0, bottom = INT_MAX;
-	for(int i = 0; i < n-1; i++) {
-		left = min(pt[i].x, left);
-		right = max(pt[i].x, right);
-		top = max(pt[i].y, top);
-		bottom = min(pt[i].y, bottom);
-	}
-	area = min(abs(right-left)*abs(top-bottom), area);
+  sort(pt, pt+n, cmpy);
 
-	sort(pt, pt+n, cmpy);
+  left = INT_MAX, right = 0;
+  top = 0, bottom = INT_MAX;
+  for(int i = 1; i < n; i++) {
+    left = min(pt[i].x, left);
+    right = max(pt[i].x, right);
+    top = max(pt[i].y, top);
+    bottom = min(pt[i].y, bottom);
+  }
 
-	left = INT_MAX, right = 0;
-	top = 0, bottom = INT_MAX;
-	for(int i = 1; i < n; i++) {
-		left = min(pt[i].x, left);
-		right = max(pt[i].x, right);
-		top = max(pt[i].y, top);
-		bottom = min(pt[i].y, bottom);
-	}
+  area = min(abs(right-left)*abs(top-bottom), area);
 
-	area = min(abs(right-left)*abs(top-bottom), area);
+  left = INT_MAX, right = 0;
+  top = 0, bottom = INT_MAX;
+  for(int i = 0; i < n-1; i++) {
+    left = min(pt[i].x, left);
+    right = max(pt[i].x, right);
+    top = max(pt[i].y, top);
+    bottom = min(pt[i].y, bottom);
+  }
 
-	left = INT_MAX, right = 0;
-	top = 0, bottom = INT_MAX;
-	for(int i = 0; i < n-1; i++) {
-		left = min(pt[i].x, left);
-		right = max(pt[i].x, right);
-		top = max(pt[i].y, top);
-		bottom = min(pt[i].y, bottom);
-	}
+  area = min(abs(right-left)*abs(top-bottom), area);
 
-	area = min(abs(right-left)*abs(top-bottom), area);
-
-	cout<<area<<endl;
-	return 0;
+  cout<<area<<endl;
+  return 0;
 } 
