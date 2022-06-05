fc7c5b0338d81d8163d48af68de730afdc536d3c
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-05 02:20:21 -0400

    Update block game v2.java

--- Bronze/2016-2017/Dec/block game v2.java
+++ Bronze/2016-2017/Dec/block game v2.java
@@ -4,7 +4,7 @@ import java.io.*;
 class Main {
   public static void main(String[] args) throws IOException{
     BufferedReader in = new BufferedReader(new FileReader("speeding.in"));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("speeding.out")));
+	PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("speeding.out")));
 
     int n = Integer.parseInt(in.readLine());
 
