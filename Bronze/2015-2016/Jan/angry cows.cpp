fe6785e6b5fde68c20458598e23fc36214e037f9
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-05 02:34:16 -0400

    Update angry cows.cpp

--- Bronze/2015-2016/Jan/angry cows.cpp
+++ Bronze/2015-2016/Jan/angry cows.cpp
@@ -4,44 +4,44 @@ using namespace std;
 int hb[101];
 
 int blast(int start, int length, bool left) {
-	int src = start, dis = 1, dir = left ? -1 : 1;
+  int src = start, dis = 1, dir = left ? -1 : 1;
+  
+  while(src >= 0 && src < length) {
+    int nxt = src;
+    while(nxt + dir >= 0 && nxt + dir < length && abs(hb[src] - hb[nxt+dir]) <= dis) {
+      nxt += dir;
+    }
+    if(src == nxt) {
+      break;
+    }
 
-	while(src >= 0 && src < length) {
-		int nxt = src;
-		while(nxt + dir >= 0 && nxt + dir < length && abs(hb[src] - hb[nxt+dir]) <= dis) {
-			nxt += dir;
-		}
-		if(src == nxt) {
-			break;
-		}
-
-		src = nxt;
-		dis++;
-	}
-	return src;
+    src = nxt;
+    dis++;
+  }
+  return src;
 }
 
 int main() {
-	freopen("angry.in", "r", stdin);
-	freopen("angry.out", "w", stdout);
+  freopen("angry.in", "r", stdin);
+  freopen("angry.out", "w", stdout);
 
-	int n;
-	cin>>n;
+  int n;
+  cin>>n;
 
-	for(int i = 0; i < n; i++) {
-		cin>>hb[i];
-	}
+  for(int i = 0; i < n; i++) {
+    cin>>hb[i];
+  }
 
-	sort(hb, hb+n);
+  sort(hb, hb+n);
 
-	int ans = 0;
-	for(int i = 0; i < n; i++) {
-		int left = blast(i, n, true);
-		int right = blast(i, n, false);
-		int temp = right - left + 1;
-		ans = max(temp, ans);
-	}
+  int ans = 0;
+  for(int i = 0; i < n; i++) {
+    int left = blast(i, n, true);
+    int right = blast(i, n, false);
+    int temp = right - left + 1;
+    ans = max(temp, ans);
+  }
 
-	cout<<ans<<endl;
-	return 0;
+  cout<<ans<<endl;
+  return 0;
 } 
