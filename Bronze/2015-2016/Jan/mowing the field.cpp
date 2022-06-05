1bf2d65fb1da67640a1081819b07d0468785638c
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-05 02:36:40 -0400

    Update mowing the field.cpp

--- Bronze/2015-2016/Jan/mowing the field.cpp
+++ Bronze/2015-2016/Jan/mowing the field.cpp
@@ -7,51 +7,52 @@ int mp2[2001][2001];
 int p[10010];
 
 int main() {
-	freopen("mowing.in", "r", stdin);
-	freopen("mowing.out", "w", stdout);
+  freopen("mowing.in", "r", stdin);
+  freopen("mowing.out", "w", stdout);
 
-	int n;
-	cin>>n;
+  int n;
+  cin>>n;
 
-	mp[1000][1000] = 1;
-	bool found = false;
-	int neg = 0, loc1 = 1000, loc2 = 1000;
-	int minimum = INT_MAX, pre = 0, sum = 0;
+  mp[1000][1000] = 1;
+  bool found = false;
+  int neg = 0, loc1 = 1000, loc2 = 1000;
+  int minimum = INT_MAX, pre = 0, sum = 0;
 	int ans = INT_MAX;
-	for(int i = 0; i < n; i++) {
-		char d;
-		int steps;
-		cin>>d>>steps;
+  for(int i = 0; i < n; i++) {
+    char d;
+    int steps;
+    cin>>d>>steps;
+    
+    for(int j = 0; j < steps; j++) {
+      sum++;
+      if(d == 'S') {
+        loc1++;
+      }
+      else if(d == 'N') {
+        loc1--;
+      }
+      else if(d == 'E') {   
+        loc2++;
+      }
+      else if(d == 'W') {
+        loc2--;
+      }
 
-		for(int j = 0; j < steps; j++) {
-			sum++;
-			if(d == 'S') {
-				loc1++;
-			}
-			else if(d == 'N') {
-				loc1--;
-			}
-			else if(d == 'E') {   
-				loc2++;
-			}
-			else if(d == 'W') {
-				loc2--;
-			}
+      if(mp[loc1][loc2] >= 1) {
+        found = true;
+        minimum = sum - mp2[loc1][loc2];
+      }
 
-			if(mp[loc1][loc2] >= 1) {
-				found = true;
-				minimum = sum - mp2[loc1][loc2];
-			}
+      mp[loc1][loc2]++;
+      mp2[loc1][loc2] = sum;
+      neg += mp[loc1][loc2];
 
-			mp[loc1][loc2]++;
-			mp2[loc1][loc2] = sum;
-			neg += mp[loc1][loc2];
+      ans = min(ans, minimum);
+    }
+    
+  }
 
-			ans = min(ans, minimum);
-		}
-	}
-
-	if(neg==sum) cout<<-1<<endl;
-	else cout<<ans<<endl;
-	return 0;
+  if(neg==sum) cout<<-1<<endl;
+  else cout<<ans<<endl;
+  return 0;
 } 
