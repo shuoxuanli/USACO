6f32f046e8d250e4e46236892e751c5687162da5
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-04 21:55:14 -0400

    Update fence painting.java

--- Bronze/2015-2016/Dec/fence painting.java
+++ Bronze/2015-2016/Dec/fence painting.java
@@ -2,47 +2,47 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-  public static void main(String[] args) throws IOException {
-    Kattio io = new Kattio("paint");
-    int a = io.nextInt();
-    int b = io.nextInt();
-    int c = io.nextInt();
-    int d = io.nextInt();
-    
-    int total = (b - a) + (d - c);
-    int intersection = Math.max(Math.min(b, d) - Math.max(a, c), 0);
-    int union = total - intersection;
+	public static void main(String[] args) throws IOException {
+		Kattio io = new Kattio("paint");
+		int a = io.nextInt();
+		int b = io.nextInt();
+		int c = io.nextInt();
+		int d = io.nextInt();
 
-    io.println(union);
-    io.close();
-  }
+		int total = (b - a) + (d - c);
+		int intersection = Math.max(Math.min(b, d) - Math.max(a, c), 0);
+		int union = total - intersection;
 
-  static class Kattio extends PrintWriter {
-    private BufferedReader r;
-    private StringTokenizer st;
-	
-    public Kattio() { this(System.in, System.out); }
-    public Kattio(InputStream i, OutputStream o) {
-      super(o);
-      r = new BufferedReader(new InputStreamReader(i));
-    }
+		io.println(union);
+		io.close();
+	}
 
-    public Kattio(String problemName) throws IOException {
-      super(new FileWriter(problemName + ".out"));
-      r = new BufferedReader(new FileReader(problemName + ".in"));
-    }
+	static class Kattio extends PrintWriter {
+		private BufferedReader r;
+		private StringTokenizer st;
 
-    public String next() {
-      try {
-        while (st == null || !st.hasMoreTokens())
-        st = new StringTokenizer(r.readLine());
-        return st.nextToken();
-      } catch (Exception e) { }
-      return null;
-    }
+		public Kattio() { this(System.in, System.out); }
+		public Kattio(InputStream i, OutputStream o) {
+			super(o);
+			r = new BufferedReader(new InputStreamReader(i));
+		}
 
-    public int nextInt() { return Integer.parseInt(next()); }
-    public double nextDouble() { return Double.parseDouble(next()); }
-    public long nextLong() { return Long.parseLong(next()); }
-  }
+		public Kattio(String problemName) throws IOException {
+			super(new FileWriter(problemName + ".out"));
+			r = new BufferedReader(new FileReader(problemName + ".in"));
+		}
+
+		public String next() {
+			try {
+				while (st == null || !st.hasMoreTokens())
+					st = new StringTokenizer(r.readLine());
+				return st.nextToken();
+			} catch (Exception e) { }
+			return null;
+		}
+
+		public int nextInt() { return Integer.parseInt(next()); }
+		public double nextDouble() { return Double.parseDouble(next()); }
+		public long nextLong() { return Long.parseLong(next()); }
+	}
 }
