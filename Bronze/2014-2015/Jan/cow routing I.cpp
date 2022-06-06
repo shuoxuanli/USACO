ce5390fe6b0f6d4e9cae3d59765e908d8fae4983
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-06 11:57:57 -0400

    Update cow routing I.cpp

--- Bronze/2014-2015/Jan/cow routing I.cpp
+++ Bronze/2014-2015/Jan/cow routing I.cpp
@@ -2,34 +2,34 @@
 using namespace std;
 
 int main() {
-	freopen("cowroute.in", "r", stdin);
-	freopen("cowroute.out", "w", stdout);
+  freopen("cowroute.in", "r", stdin);
+  freopen("cowroute.out", "w", stdout);
 
-	int n, a, b;
-	cin>>a>>b>>n;
+  int n, a, b;
+  cin>>a>>b>>n;
 
-	int ans = INT_MAX;
-	for(int i = 0; i < n; i++) {
-		int cost, m;
-		cin>>cost>>m;
-		int tmp = -1;
-		int tmp2 = -1;
-		for(int j = 0; j < m; j++) {
-			int city;
-			cin>>city;
-			if(city == a) {
-				tmp = j;
-			} 
-			else if(city == b) {
-				tmp2 = j;
-			}
-		}
-		if(tmp < tmp2 && tmp != -1 && tmp2 != -1) {
-			ans = min(ans, cost);
-		}
-	}
+  int ans = INT_MAX;
+  for(int i = 0; i < n; i++) {
+    int cost, m;
+    cin>>cost>>m;
+    int tmp = -1;
+    int tmp2 = -1;
+    for(int j = 0; j < m; j++) {
+      int city;
+      cin>>city;
+      if(city == a) {
+        tmp = j;
+      } 
+      else if(city == b) {
+        tmp2 = j;
+      }
+    }
+    if(tmp < tmp2 && tmp != -1 && tmp2 != -1) {
+      ans = min(ans, cost);
+    }
+  }
 
-	ans = (ans==INT_MAX) ? -1:ans;
-	cout<<ans<<endl;
-	return 0;
+  ans = (ans==INT_MAX) ? -1:ans;
+  cout<<ans<<endl;
+  return 0;
 }
