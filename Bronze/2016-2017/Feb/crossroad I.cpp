d2fb3ad3845aceca2ab8b385f70c6dcf50cb8956
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-06-08 14:42:22 -0400

    Update crossroad I.cpp

--- Bronze/2016-2017/Feb/crossroad I.cpp
+++ Bronze/2016-2017/Feb/crossroad I.cpp
@@ -1,37 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool grid[10][10];
-
 int main() {
-  freopen("cowtip.in", "r", stdin);
-  freopen("cowtip.out", "w", stdout);
+  freopen("crossroad.in", "r", stdin);
+  freopen("crossroad.out", "w", stdout);
 
   int n;
   cin>>n;
 
-  string s; 
-  for(int i = 0; i < n; i++) {
-    cin>>s;
-    for(int j = 0; j < n; j++) {
-      grid[i][j] = s[j] - '0';
-    }
-  }
+  map<int, int> map;
 
   int ans = 0;
-  for(int i = n-1; i >= 0; i--) {
-    for(int j = n-1; j >= 0; j--) {
-      if(grid[i][j]) {
-        ans++;
-        for(int k = 0; k <= i; k++) {
-          for(int m = 0; m <= j; m++) {
-            grid[k][m] = !grid[k][m];
-          }     
-        }
-      }
-    }
+  for(int i = 0; i < n; i++) {
+   int cow, road;
+   cin>>cow>>road;
+   if(map.count(cow) && road != map[cow]) {
+     ans++;
+   }
+   map[cow] = road;
   }
 
   cout<<ans<<endl;
   return 0;
-}
+} 
