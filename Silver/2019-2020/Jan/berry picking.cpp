e7c16d5454c4147835964d44e536eb2538044a03
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-07-27 09:12:59 -0400

    Update berry picking.cpp

--- Silver/2019-2020/Jan/berry picking.cpp
+++ Silver/2019-2020/Jan/berry picking.cpp
@@ -25,18 +25,18 @@ int main() {
     int full = 0;
     for(int i=0;i<n;i++)
       full += a[i]/b;
-      if(full < k/2)
-	break;
-      if(full >= k) {
-        best = max(best, b*(k/2));
-	continue;
-      }
-      mod = b;
-      sort(a, a+n, cmp);
-      int cur = b*(full - k/2);
-      for(int i=0;i<n && i+full<k;i++)
-        cur += a[i]%b;
-	best = max(best,cur);
-      }
-   cout << best << '\n';
+    if(full < k/2)
+      break;
+    if(full >= k) {
+      best = max(best, b*(k/2));
+      continue;
+    }
+    mod = b;
+    sort(a, a+n, cmp);
+    int cur = b*(full - k/2);
+    for(int i=0;i<n && i+full<k;i++)
+      cur += a[i]%b;
+    best = max(best,cur);
+  }
+  cout << best << '\n';
 }
