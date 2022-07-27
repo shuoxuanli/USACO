aa2e069cf6c71ab15da3bfd6de7cdec3573376fb
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-07-27 09:19:08 -0400

    Update stuck in a rut.cpp

--- Silver/2020-2021/Dec/stuck in a rut.cpp
+++ Silver/2020-2021/Dec/stuck in a rut.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-
 #define FOR(i, a, b) for(int i = (a); i < (b); i++)
 #define FORE(i, a, b) for(int i = (a); i <= (b); i++)
 #define F0R(i, a) for(int i = 0; i < (a); i++)
@@ -39,21 +38,22 @@ bool cmp2(const Cow &o, const Cow &t){
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-
+  
   cin >> N;
-
+  
   F0R(i, N){
     int x, y; char d; cin>>d>>x>>y;
     if(d=='N'){
       C_N.push_back(Cow({x,y},i));
-    } else{
+    }
+    else{
       C_E.push_back(Cow({x,y},i));
     }
   }
-
+  
   sort(C_N.begin(), C_N.end(), cmp);
   sort(C_E.begin(), C_E.end(), cmp2);
-
+  
   bool stopped[2501];
   int blame[2501];
   fill(stopped, stopped+N, false);
@@ -61,17 +61,17 @@ int main() {
   trav(c1, C_E){
     trav(c2, C_N){
       if(!stopped[c1.id] && !stopped[c2.id] && c1.coor.second >= c2.coor.second && c1.coor.first <= c2.coor.first){
-	int xlen = c2.coor.first - c1.coor.first;
-	int ylen = c1.coor.second - c2.coor.second;
+        int xlen = c2.coor.first - c1.coor.first;
+        int ylen = c1.coor.second - c2.coor.second;
         
         if(ylen < xlen){
-	  stopped[c1.id] = true;
-	  blame[c2.id] += 1 + blame[c1.id];
-	}
-	else if(ylen > xlen){
-	  stopped[c2.id] = true;
-	  blame[c1.id] += 1 + blame[c2.id];
-	}
+          stopped[c1.id] = true;
+          blame[c2.id] += 1 + blame[c1.id];
+        }
+        else if(ylen > xlen){
+          stopped[c2.id] = true;
+          blame[c1.id] += 1 + blame[c2.id];
+        }
       }
     }
   }
