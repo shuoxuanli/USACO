b689e77cce899dee12551a52e0c22a59c3682322
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-07-27 09:25:38 -0400

    Update visits.cpp

--- Silver/2021-2022/Open/visits.cpp
+++ Silver/2021-2022/Open/visits.cpp
@@ -9,35 +9,35 @@ bool vis[100001];
 ll ans = 0;
 
 void cycle(int u) {
-	int mn = INT_MAX;
-	while(!vis[u]) {
-		vis[u] = 1;
-		ans += v[u];
-		mn = min(mn, v[u]);
-		u = a[u];
-	}
-	if(mn != INT_MAX)
-		ans -= mn;
+  int mn = INT_MAX;
+  while(!vis[u]) {
+    vis[u] = 1;
+    ans += v[u];
+    mn = min(mn, v[u]);
+    u = a[u];
+  }
+  if(mn != INT_MAX)
+    ans -= mn;
 }
 
 void del(int u) {
-	vis[u] = 1;
-	ans += v[u];
-	id[a[u]]--;
-	if(!id[a[u]] && !vis[a[u]]) 
-		del(a[u]);
+  vis[u] = 1;
+  ans += v[u];
+  id[a[u]]--;
+  if(!id[a[u]] && !vis[a[u]]) 
+    del(a[u]);
 }
 
 int main() {
-	cin>>n;
-	for(int i = 1; i <= n; i++) 
-		cin>>a[i]>>v[i], id[a[i]]++;
-	for(int i = 1; i <= n; i++) {
-		if(!id[i] && !vis[i]) del(i);
-	}
-	for(int i = 1; i <= n; i++) {
-		cycle(i);
-	}
-	cout<<ans<<endl;
-	return 0;
+  cin>>n;
+  for(int i = 1; i <= n; i++) 
+    cin>>a[i]>>v[i], id[a[i]]++;
+  for(int i = 1; i <= n; i++) {
+    if(!id[i] && !vis[i]) del(i);
+  }
+  for(int i = 1; i <= n; i++) {
+    cycle(i);
+  }
+  cout<<ans<<endl;
+  return 0;
 }
