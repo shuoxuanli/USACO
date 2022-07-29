a29f3915083f73746df2e8a0b21cd9b9fa3301a4
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-07-28 22:44:59 -0400

    Update lasers and mirrors.cpp

--- Gold/2016-2017/Dec/lasers and mirrors.cpp
+++ Gold/2016-2017/Dec/lasers and mirrors.cpp
@@ -2,39 +2,39 @@
 using namespace std;
 
 int main() {
-	freopen("lasers.in", "r", stdin);
-	freopen("lasers.out", "w", stdout);
-	int n; cin >> n;
+  freopen("lasers.in", "r", stdin);
+  freopen("lasers.out", "w", stdout);
+  int n; cin >> n;
   
-	vector<pair<int, int>> points(n + 2);
+  vector<pair<int, int>> points(n + 2);
   
-	unordered_map<int, vector<int>> lines[2];
+  unordered_map<int, vector<int>> lines[2];
   
-	for (int i = 0; i < n + 2; i++) {
-		cin >> points[i].first >> points[i].second;
-		lines[0][points[i].first].push_back(i);
-		lines[1][points[i].second].push_back(i);
-	}
-
-	queue<pair<int, bool>> q;
-	q.push({0, true});
-	q.push({0, false});
+  for (int i = 0; i < n + 2; i++) {
+    cin >> points[i].first >> points[i].second;
+    lines[0][points[i].first].push_back(i);
+    lines[1][points[i].second].push_back(i);
+  }
   
-	vector<int> dist(n + 2, 1e9);
-	dist[0] = 0;
-	while (!q.empty()) {
-		int curr = q.front().first;
-		bool beamdir = q.front().second;
-		q.pop();
-		int dir = (beamdir ? 0 : 1);
-		int coord = (beamdir ? points[curr].first : points[curr].second);
-		for (int point : lines[dir][coord]) {
-			if (dist[point] == 1e9) {
-				q.push({point, !beamdir});
-				dist[point] = dist[curr] + 1;
-			}
-		}
-	}
-	cout << (dist[1] == 1e9 ? -1 : dist[1] - 1) << endl;
-	return 0;
+  queue<pair<int, bool>> q;
+  q.push({0, true});
+  q.push({0, false});
+  
+  vector<int> dist(n + 2, 1e9);
+  dist[0] = 0;
+  while (!q.empty()) {
+    int curr = q.front().first;
+    bool beamdir = q.front().second;
+    q.pop();
+    int dir = (beamdir ? 0 : 1);
+    int coord = (beamdir ? points[curr].first : points[curr].second);
+    for (int point : lines[dir][coord]) {
+      if (dist[point] == 1e9) {
+        q.push({point, !beamdir});
+        dist[point] = dist[curr] + 1;
+      }
+    }
+  }
+  cout << (dist[1] == 1e9 ? -1 : dist[1] - 1) << endl;
+  return 0;
 }
