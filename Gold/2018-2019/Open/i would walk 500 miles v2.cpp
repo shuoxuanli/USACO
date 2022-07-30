07f6297d1b081b5911e1baf0871b717e864532f2
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-07-29 23:31:20 -0400

    Create i would walk 500 miles v2.cpp

---
+++ Gold/2018-2019/Open/i would walk 500 miles v2.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ifstream cin("walk.in");
+	ofstream cout("walk.out");
+	int n, k;
+	cin>>n>>k;
+	cout<<2019201997-48*n-84*(k-1)<<endl;
+}
