7b15f3189e2a4e2898dc7c05729ab5d2890923b5
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-07-29 23:31:37 -0400

    Update i would walk 500 miles v2.cpp

--- Gold/2018-2019/Open/i would walk 500 miles v2.cpp
+++ Gold/2018-2019/Open/i would walk 500 miles v2.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-	ifstream cin("walk.in");
-	ofstream cout("walk.out");
-	int n, k;
-	cin>>n>>k;
-	cout<<2019201997-48*n-84*(k-1)<<endl;
+  ifstream cin("walk.in");
+  ofstream cout("walk.out");
+  int n, k;
+  cin>>n>>k;
+  cout<<2019201997-48*n-84*(k-1)<<endl;
 }
