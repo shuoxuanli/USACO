de9790c767ba34fb831f4da607986a734df36086
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-07-29 23:19:54 -0400

    Update cowpatibility.cpp

--- Gold/2018-2019/Dec/cowpatibility.cpp
+++ Gold/2018-2019/Dec/cowpatibility.cpp
@@ -10,21 +10,19 @@ int main() {
   freopen("cowpatibility.in", "r", stdin);
   freopen("cowpatibility.out", "w", stdout);
   
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-
   cin>>n;
-	ans=n*(n-1)/2;
-	for (int c=1; c<=n; c++) {
+  
+  ans=n*(n-1)/2;
+  for (int c=1; c<=n; c++) {
     for (int i=1;i<=5;i++) {
       cin>>a[i];
     }
-		sort(a+1,a+6);
-		LL sum=0;
-		for (int i=1; i<=31; i++) {
-			int cnt=0;
+    sort(a+1,a+6);
+    LL sum=0;
+    for (int i=1; i<=31; i++) {
+      int cnt=0;
       string key="";
-		  for(int j = 1; j <= 5; j++) {
+      for(int j = 1; j <= 5; j++) {
         if(i&(1<<(j-1))) {
           cnt++;
           key += " " + a[j];
