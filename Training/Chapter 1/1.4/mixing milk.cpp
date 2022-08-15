6597788540c6365e8cf7d612a48c8b3ca0a293ed
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-08-15 19:35:53 -0400

    Create mixing milk.cpp

---
+++ Training/Chapter 1/1.4/mixing milk.cpp
@@ -0,0 +1,46 @@
+/*
+ID: Shuoxuan Li [shuoxua2]
+LANG: C++
+TASK: milk
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct farmer{
+  int price, unit;
+};
+
+farmer milk[5001];
+
+bool cmp(farmer a, farmer b) {
+  return a.price < b.price;
+}
+int main() {
+  freopen("milk.in", "r", stdin);
+  freopen("milk.out", "w", stdout);
+
+  int n, m;
+  cin>>n>>m;
+
+  int money = 0;
+  for(int i = 0; i < m; i++) {
+    cin>>milk[i].price>>milk[i].unit;
+  }
+
+  sort(milk, milk + m, cmp);
+
+  for(int i = 0; i < m; i++) {
+    if(n > milk[i].unit) {
+      n -= milk[i].unit;
+      money += milk[i].price * milk[i].unit;
+    }
+    else {
+      money += n*milk[i].price;
+      break;
+    }
+  }
+
+  cout<<money<<endl;
+  return 0;
+} 
