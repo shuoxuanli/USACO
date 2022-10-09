bbecbd0d18a7cc7dfe9ad95889dcce7765a3ba1d
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-10-09 02:43:10 -0400

    Update lasers and mirrors.cpp

--- Gold/2016-2017/Dec/lasers and mirrors.cpp
+++ Gold/2016-2017/Dec/lasers and mirrors.cpp
@@ -1,40 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+#define sz(x) (int)(x.size())
+
+int n;
+int x[100005], y[100005], dis[100005];
+map<int, vector<int>> adj[2];
+
 int main() {
-  freopen("lasers.in", "r", stdin);
-  freopen("lasers.out", "w", stdout);
-  int n; cin >> n;
+  ifstream cin("lasers.in");
+  ofstream cout("lasers.out");
   
-  vector<pair<int, int>> points(n + 2);
+  cin>>n>>x[0]>>y[0]>>x[n+1]>>y[n+1];
   
-  unordered_map<int, vector<int>> lines[2];
-  
-  for (int i = 0; i < n + 2; i++) {
-    cin >> points[i].first >> points[i].second;
-    lines[0][points[i].first].push_back(i);
-    lines[1][points[i].second].push_back(i);
+  for(int i = 1; i <= n; i++) {
+    cin>>x[i]>>y[i];
   }
   
-  queue<pair<int, bool>> q;
-  q.push({0, true});
-  q.push({0, false});
+  for(int i = 0; i <= n+1; i++) {
+    adj[0][x[i]].push_back(i);
+    adj[1][y[i]].push_back(i);
+  }
   
-  vector<int> dist(n + 2, 1e9);
-  dist[0] = 0;
-  while (!q.empty()) {
-    int curr = q.front().first;
-    bool beamdir = q.front().second;
-    q.pop();
-    int dir = (beamdir ? 0 : 1);
-    int coord = (beamdir ? points[curr].first : points[curr].second);
-    for (int point : lines[dir][coord]) {
-      if (dist[point] == 1e9) {
-        q.push({point, !beamdir});
-        dist[point] = dist[curr] + 1;
+  memset(dis, 0x3f, sizeof(dis));
+  queue<pair<int, int>> q;
+  q.push({0, 0});
+  q.push({0, 1});
+  dis[0] = 0;
+  
+  while(!q.empty()) {
+    int u, d;
+    tie(u, d) = q.front(), q.pop();
+  
+    int k = (d == 0 ? x[u] : y[u]);
+    for(int v : adj[d][k]) {
+      if(dis[u] + 1 < dis[v]) {
+        q.push({v, d^1});
+        dis[v] = dis[u] + 1;
       }
     }
   }
-  cout << (dist[1] == 1e9 ? -1 : dist[1] - 1) << endl;
+  
+  cout<<(dis[n+1] == 0x3f3f3f3f ? -1 : dis[n+1] - 1)<<'\n';
   return 0;
 }
