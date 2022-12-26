9b74a22aa707af970a49a3e043b78e601a5d8338
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-12-26 01:52:05 -0500

    Update strongest friendship group.cpp

--- Gold/2022-2023/Dec/strongest friendship group.cpp
+++ Gold/2022-2023/Dec/strongest friendship group.cpp
@@ -7,7 +7,7 @@ using namespace std;
 template <class T> struct dsu {
   vector<int> f;
   vector<T> rank;
-  dsu(int n): f(n+1), rank(n+1, 1) { 
+  dsu(int n) : f(n + 1), rank(n + 1, 1) { 
     iota(f.begin(), f.end(), 0); 
   }
   int find(int x) { 
@@ -24,7 +24,7 @@ template <class T> struct dsu {
   bool same(int x, int y) { return find(x) == find(y); }
 };
 
-int n, m, siz[200005];
+int n, m, deg[200005];
 bool vis[200005], rem[200005], e[200005];
 vector<int> adj[200005];
 
@@ -35,54 +35,35 @@ int main() {
     cin >> u >> v;
     adj[u].push_back(v);
     adj[v].push_back(u);
-    siz[u]++, siz[v]++;
+    deg[u]++, deg[v]++;
   }
 
-  int g = 0;
   using pll = pair<ll, ll>;
   priority_queue<pll, vector<pll>, greater<pll>> pq;
+  for (int i = 1; i <= n; i++) pq.push({deg[i], i});
 
-  function<void(int)> dfs = [&](int u) {
-    g++;
-    vis[u] = true;
-    pq.push({siz[u], u});
-    for (int v : adj[u]) if (!vis[v]) {
-      dfs(v);
+  vector<pll> ord;
+  while (!pq.empty()) {
+    ll w, u;
+    tie(w, u) = pq.top();
+    pq.pop();
+    if (rem[u]) continue;
+    rem[u] = true;
+    for (int v : adj[u]) {
+      pq.push({--deg[v], v});
     }
-  };
-
-  auto get = [&]() {
-    vector<pll> v;
-    while (!pq.empty() && g) {
-      ll w, u;
-      tie(w, u) = pq.top();
-      pq.pop();
-      if (rem[u]) continue;
-      rem[u] = true;
-      for (int v : adj[u]) {
-        pq.push({--siz[v], v});
-      }
-      v.push_back({u, w});
-      g--;
-    }
-    while (sz(pq)) pq.pop();
-    reverse(v.begin(), v.end());
-    return v;
-  };
+    ord.push_back({u, w});
+  }
+  reverse(ord.begin(), ord.end());
 
   ll ans = 0;
   dsu<int> d(n);
-  for (int i = 1; i <= n; i++) if (!vis[i]) {
-    g = 0;
-    dfs(i);
-    auto ord = get();
-    for (auto [u, w] : ord) {
-      e[u] = true;
-      for (int v : adj[u]) if (e[v]) {
-        d.join(u, v);
-      }
-      ans = max(ans, w * d.size(u));
+  for (auto [u, w] : ord) {
+    e[u] = true;
+    for (int v : adj[u]) if (e[v]) {
+      d.join(u, v);
     }
+    ans = max(ans, w * d.size(u));
   }
 
   cout << ans << '\n';
