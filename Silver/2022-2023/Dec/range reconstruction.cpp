0923ec8724e96bddbadf46a8f5e3ff43b37d36c5
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2022-12-26 11:59:14 -0500

    Create range reconstruction.cpp

---
+++ Silver/2022-2023/Dec/range reconstruction.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define sz(x) (int)(x.size())
+
+int n, a[305][305], ans[305];
+
+int main() {
+  cin >> n;
+  for (int i = 0; i < n; i++)
+    for (int j = i; j < n; j++)
+      cin >> a[i][j];
+
+  for (int i = 1; i < n; i++) {
+    bool ok = true;
+    ans[i] = ans[i - 1] + a[i - 1][i];
+    int mx = ans[i], mn = ans[i];
+    for (int j = i - 1; j >= 0; j--) {
+      mn = min(mn, ans[j]);
+      mx = max(mx, ans[j]);
+      ok &= mx - mn == a[j][i];
+    }
+    if (!ok) ans[i] = ans[i - 1] - a[i - 1][i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    cout << ans[i] << " \n"[i == n - 1];
+  }
+  return 0;
+}
