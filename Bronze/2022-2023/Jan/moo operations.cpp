c32f06e514eb2407341228e065bac0ef4323a973
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-02-04 15:40:58 -0500

    Create moo operations.cpp

---
+++ Bronze/2022-2023/Jan/moo operations.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define sz(x) (int)(x.size())
+
+int q;
+string s;
+
+int main() {
+  cin >> q;
+  while (q--) {
+    cin >> s;
+
+    int ans = INT_MAX;
+    for (int i = 1; i < sz(s) - 1; i++) if (s[i] == 'O') {
+      ans = min(ans, sz(s) - 3 + (s[i - 1] != 'M') + (s[i + 1] != 'O'));
+    }
+
+    cout << (ans == INT_MAX ? -1 : ans) << '\n';
+  }
+  return 0;
+}
