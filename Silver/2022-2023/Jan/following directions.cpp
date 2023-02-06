a5e604bf76e8e8a7a31843dcac948705e0901997
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-02-05 22:12:49 -0500

    Update following directions.cpp

--- Silver/2022-2023/Jan/following directions.cpp
+++ Silver/2022-2023/Jan/following directions.cpp
@@ -14,21 +14,21 @@ int main() {
     cin >> s[i] >> r[i];
   for (int i = 0; i < n; i++)
     cin >> c[i];
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      cnt[i][j]++;
-      if (s[i][j] == 'R') {
-        cnt[i][j+1] += cnt[i][j];
-      } else {
-        cnt[i+1][j] += cnt[i][j];
-      }
-    }
-  }
 
   int ans = 0;
-  for (int i = 0; i < n; i++) {
-    ans += cnt[i][n] * r[i];
-    ans += cnt[n][i] * c[i];
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= n; j++) {
+      if (i == n || j == n) {
+        ans += cnt[i][j] * (i == n ? c[j] : r[i]);
+        continue;
+      }
+      cnt[i][j]++;
+      if (s[i][j] == 'R') {
+        cnt[i][j + 1] += cnt[i][j];
+      } else {
+        cnt[i + 1][j] += cnt[i][j];
+      }
+    }
   }
   cout << ans << '\n';
 
@@ -37,28 +37,23 @@ int main() {
     int x, y;
     cin >> x >> y, --x, --y;
 
-    int pre, now;
-    int i = x, j = y, t = cnt[x][y];
-    while (1) {
-      cnt[i][j] -= t;
-      if (i == n || j == n) {
-        pre = (i == n) ? c[j] : r[i];
-        break;
-      }
+    int i = x, j = y;
+    while (i != n && j != n) {
       (s[i][j] == 'D') ? i++ : j++;
+      cnt[i][j] -= cnt[x][y];
+      if (i == n || j == n) {
+        ans -= cnt[x][y] * (i == n ? c[j] : r[i]);
+      }
     }
     s[x][y] = (s[x][y] == 'R') ? 'D' : 'R';
     i = x, j = y;
-    while (1) {
-      cnt[i][j] += t;
-      if (i == n || j == n) {
-        now = (i == n) ? c[j] : r[i];
-        break;
-      }
+    while (i != n && j != n) {
       (s[i][j] == 'D') ? i++ : j++;
+      cnt[i][j] += cnt[x][y];
+      if (i == n || j == n) {
+        ans += cnt[x][y] * (i == n ? c[j] : r[i]);
+      }
     }
-    ans -= cnt[x][y] * pre;
-    ans += cnt[x][y] * now;
 
     cout << ans << '\n';
   }
