eed774550536ed7859866e1a99b19f85bae53789
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-02-13 18:17:07 -0500

    Update triangles.cpp

--- Silver/2019-2020/Feb/triangles.cpp
+++ Silver/2019-2020/Feb/triangles.cpp
@@ -9,64 +9,64 @@ int n;
 ll sx[100005], sy[100005];
 
 struct pt {
-	int x, y;
+  int x, y;
 } a[100005];
 
 int main() {
-	ifstream cin("triangles.in");
-	ofstream cout("triangles.out");
+  ifstream cin("triangles.in");
+  ofstream cout("triangles.out");
 
-	cin >> n;
+  cin >> n;
 
-	map<pair<int, int>, int> mp;
-	for (int i = 1; i <= n; i++) {
-		cin >> a[i].x >> a[i].y;
-		mp[{a[i].x, a[i].y}] = i;
-	}
+  map<pair<int, int>, int> mp;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i].x >> a[i].y;
+    mp[{a[i].x, a[i].y}] = i;
+  }
 
-	sort(a + 1, a + 1 + n, [](const pt &l, const pt &r) {
-		return l.x < r.x || (l.x == r.x && l.y < r.y);
-	});
-	for (int i = 1; i <= n; i++) {
-		int j = i;
-		ll sum = 0;
-		while (a[i].x == a[j].x) {
-			sum += a[j].y - a[i].y;
-			j++;
-		}
-		j--;
-		for (int k = i; k <= j; k++) {
-			sum += (k - i) * (a[k].y - a[k - 1].y);
-			sx[mp[{a[k].x, a[k].y}]] = sum;
-			sum -= (j - k) * (a[k + 1].y - a[k].y);
-		}
-		i = j;
-	}
+  sort(a + 1, a + 1 + n, [](const pt &l, const pt &r) {
+    return l.x < r.x || (l.x == r.x && l.y < r.y);
+  });
+  for (int i = 1; i <= n; i++) {
+    int j = i;
+    ll sum = 0;
+    while (a[i].x == a[j].x) {
+      sum += a[j].y - a[i].y;
+      j++;
+    }
+    j--;
+    for (int k = i; k <= j; k++) {
+      sum += (k - i) * (a[k].y - a[k - 1].y);
+      sx[mp[{a[k].x, a[k].y}]] = sum;
+      sum -= (j - k) * (a[k + 1].y - a[k].y);
+    }
+    i = j;
+  }
 
-	sort(a + 1, a + 1 + n, [](const pt &l, const pt &r) {
-		return l.y < r.y || (l.y == r.y && l.x < r.x);
-	});
-	for (int i = 1; i <= n; i++) {
-		int j = i;
-		ll sum = 0;
-		while (a[i].y == a[j].y) {
-			sum += a[j].x - a[i].x;
-			j++;
-		}
-		j--;
-		for (int k = i; k <= j; k++) {
-			sum += (k - i) * (a[k].x - a[k - 1].x);
-			sy[mp[{a[k].x, a[k].y}]] = sum;
-			sum -= (j - k) * (a[k + 1].x - a[k].x);
-		}
-		i = j;
-	}
+  sort(a + 1, a + 1 + n, [](const pt &l, const pt &r) {
+    return l.y < r.y || (l.y == r.y && l.x < r.x);
+  });
+  for (int i = 1; i <= n; i++) {
+    int j = i;
+    ll sum = 0;
+    while (a[i].y == a[j].y) {
+      sum += a[j].x - a[i].x;
+      j++;
+    }
+    j--;
+    for (int k = i; k <= j; k++) {
+      sum += (k - i) * (a[k].x - a[k - 1].x);
+      sy[mp[{a[k].x, a[k].y}]] = sum;
+      sum -= (j - k) * (a[k + 1].x - a[k].x);
+    }
+    i = j;
+  }
 
-	int ans = 0;
-	for (int i = 1; i <= n; i++) {
-		ans = (ans + 1LL * sx[i] * sy[i] % MOD) % MOD;
-	}
+  int ans = 0;
+  for (int i = 1; i <= n; i++) {
+    ans = (ans + 1LL * sx[i] * sy[i] % MOD) % MOD;
+  }
 
-	cout << ans << '\n';
-	return 0;
+  cout << ans << '\n';
+  return 0;
 }
