c7b64d913f206ea45491ca12a71c0a84b91479a4
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2023-03-05 11:08:29 -0500

    Create hungry cow.cpp

---
+++ Bronze/2022-2023/Feb/hungry cow.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define sz(x) (int)(x.size())
+
+ll n, t;
+
+int main() {
+  cin >> n >> t;
+
+  ll ans = 0, st = 0, ed = 0;
+  for (int i = 0; i < n; i++) {
+    ll d, b;
+    cin >> d >> b;
+    if (d > ed) {
+      ans += ed - st;
+      st = d;
+      ed = d + b;
+    } else {
+      ed += b;
+    }
+  }
+  ed = min(ed, t + 1);
+  ans += ed - st;
+
+  cout << ans << '\n';
+}
