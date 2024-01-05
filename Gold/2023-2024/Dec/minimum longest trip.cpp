74d3c8437a59361f97069ac6a02f960a1122fb6e
Shuoxuan Li <103616713+shuoxuanli@users.noreply.github.com>
2024-01-05 17:12:01 +0800

    Update minimum longest trip.cpp

--- Gold/2023-2024/Dec/minimum longest trip.cpp
+++ Gold/2023-2024/Dec/minimum longest trip.cpp
@@ -29,28 +29,19 @@ int main() {
 
     while (!q0.empty()) {
         vector<int> q1;
-        for (int u : q0) {
+        for (int i = 0; i < sz(q0); i++) {
+            int u = q0[i];
             for (auto [v, w] : adj[u]) {
-                path[v].push_back({ans[u][0] + 1, -w, -ans[u][2], ans[u][3] + w});
+                ans[v] = max(ans[v], {ans[u][0] + 1, -w, -i, ans[u][3] + w});
                 if (!(--deg[v])) {
                     q1.push_back(v);
                 }
             }
         }
         swap(q0, q1);
-
-        vector<int> v;
-        for (int u : q0) {
-            ans[u] = *max_element(path[u].begin(), path[u].end());
-            v.push_back(u);
-        }
-        sort(v.begin(), v.end(), [&](int u, int v) {
+        sort(q0.begin(), q0.end(), [&](int u, int v) {
             return ans[u] > ans[v];
         });
-
-        for (int i = 0; i < sz(v); i++) {
-            ans[v[i]][2] = i;
-        }
     }
 
     for (int i = 1; i <= n; i++) {
